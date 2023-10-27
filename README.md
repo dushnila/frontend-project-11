c257026eefe3098d250dc0e7b924a0f6968d8976
dushnila <psihochudovishe@gmail.com>
2023-10-28 00:16:08 +0300

    step1 It seems to be working.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/dushnila/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/dushnila/frontend-project-11/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/9ac27ea11ea9a42f8ba9/maintainability)](https://codeclimate.com/github/dushnila/frontend-project-11/maintainability)
+
+https://frontend-project-11-two-pi.vercel.app/
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,46 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV === 'production';
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
+const outputPath = path.resolve(dirname, 'dist');
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+  entry: './src/index.js',
+  output: {
+    path: outputPath,
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // Добавьте свои плагины здесь
+    // Узнайте больше о плагинах по адресу https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      // Добавьте свои правила для пользовательских модулей здесь
+      // Узнайте больше о загрузчиках по адресу https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+  },
+  mode: isProduction ? 'production' : 'development',
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
+export default config;
